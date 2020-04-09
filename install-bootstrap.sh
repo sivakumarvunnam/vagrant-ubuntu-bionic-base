31212a8a5118138984ac2d2ab134c9baa625d103
sivakumar vunnam <sivakumarvunnam1@gmail.com>
2020-04-09 21:10:27 +0000

    added java to bootstrap

--- install-bootstrap.sh
+++ install-bootstrap.sh
@@ -44,7 +44,22 @@ function installAnsible() {
   fi
 }
 
+function installJava() {
+    if ! [ -x "$(command -v java)" ]; then
+    echo 'Error: Java is not installed.' >&2
+ else
+    echo "Instaling Java..."
+    sudo add-apt-repository ppa:webupd8team/java
+    sudo apt-get update
+    sudo apt-get install -y openjdk-8-jre-headless
+    JAVA_HOME=`readlink -e $(which java)`
+    export PATH=$PATH:$JAVA_HOME
+    echo "Java home : ${JAVA_HOME}"
+  fi
+}
+
 installDocker
 installDockerCompose
 installAwsCli
 installAnsible
+installJava
